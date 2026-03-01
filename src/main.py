666d17a5bd1af59a8eda14e8fa48f405000ce8aa
PaStepien <pstepien@interia.pl>
2026-03-01 21:25:02 +0100

    added the model training

--- src/main.py
+++ src/main.py
@@ -3,58 +3,67 @@ import pandas as pd
 from bolus_feature_engineering import add_insulin_activity
 from data_handling import clean_and_summarise_patients_data, parse_and_combine_patients, parse_and_combine_patients_bolus, parse_and_combine_patients_meals
 from meals_feature_engineering import add_meal_activity
+from model_handling import read_csv_for_modeling, train_pateint_model
 from parser import add_bolus_raw, add_meal_data, parse_dataframe_to_csv, add_basis_steps
 from steps_feature_engineering import add_steps_weighted_avg
 from data_handling import clean_and_summarise_patients_data, parse_and_combine_patients, parse_and_combine_patients_bolus, parse_and_combine_patients_basis_steps
 from validation import check_bolus_dose
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-data_folder = os.getenv("DATA_PATH")
-
-processed_patients = []
-processed_patients_bolus = []
-
-combined_df = parse_and_combine_patients(data_folder, processed_patients)
-cleaned_df = clean_and_summarise_patients_data(combined_df)
+data_folder = os.getenv("MODEL_DATA_PATH")
+model_data_folder = os.getenv("MODEL_RESULTS_PATH")
+df = read_csv_for_modeling(data_folder)
 
 
-bolus_df = parse_and_combine_patients_bolus(data_folder, processed_patients_bolus)
-bolus_and_glucose_df = add_bolus_raw(cleaned_df, bolus_df)
+train_pateint_model(df, model_data_folder)
+
+# processed_patients = []
+# processed_patients_bolus = []
+
+# combined_df = parse_and_combine_patients(data_folder, processed_patients)
+# cleaned_df = clean_and_summarise_patients_data(combined_df)
 
 
-check_bolus_dose(bolus_and_glucose_df)
-bolus = add_insulin_activity(bolus_and_glucose_df)
-
-meal_df =parse_and_combine_patients_meals(data_folder)
-steps_df = parse_and_combine_patients_basis_steps(data_folder)
-
-print(meal_df.head())
-
-bolus_and_meal_df = add_meal_data(bolus, meal_df)
-# print(bolus_and_glucose_df[bolus_and_glucose_df["bolus_raw"] > 0])
-output_folder = os.getenv("OUTPUT_PATH")
-meal = add_meal_activity(bolus_and_meal_df)
+# bolus_df = parse_and_combine_patients_bolus(data_folder, processed_patients_bolus)
+# bolus_and_glucose_df = add_bolus_raw(cleaned_df, bolus_df)
 
 
-bolus_and_steps_df = add_basis_steps(meal, steps_df)
+# check_bolus_dose(bolus_and_glucose_df)
+# bolus = add_insulin_activity(bolus_and_glucose_df)
 
-# Add steps_weighted_avg feature
-bolus_and_steps_df = add_steps_weighted_avg(bolus_and_steps_df, steps_col='steps')
+# meal_df =parse_and_combine_patients_meals(data_folder)
+# steps_df = parse_and_combine_patients_basis_steps(data_folder)
 
-df_to_save = bolus_and_steps_df.sort_values(["patient_id", "timestamp"])
-df_to_save = df_to_save.reset_index()
+# print(meal_df.head())
 
-parse_dataframe_to_csv(output_folder, df_to_save)
-# parse_dataframe_to_parquet(output_folder, df_to_save)
+# bolus_and_meal_df = add_meal_data(bolus, meal_df)
+# # print(bolus_and_glucose_df[bolus_and_glucose_df["bolus_raw"] > 0])
+# output_folder = os.getenv("OUTPUT_PATH")
+# meal = add_meal_activity(bolus_and_meal_df)
 
 
-print("steps_df head:", steps_df.head())
-print("cleaned_df head:", cleaned_df.head())
+# bolus_and_steps_df = add_basis_steps(meal, steps_df)
 
-# Merge bolus_raw into steps_and_glucose_df, preserving steps_raw
-parse_dataframe_to_csv(output_folder, df_to_save)
+# # Add steps_weighted_avg feature
+# bolus_and_steps_df = add_steps_weighted_avg(bolus_and_steps_df, steps_col='steps')
+
+# df_to_save = bolus_and_steps_df.sort_values(["patient_id", "timestamp"])
+# df_to_save = df_to_save.reset_index()
+
+# parse_dataframe_to_csv(output_folder, df_to_save)
+# # parse_dataframe_to_parquet(output_folder, df_to_save)
+
+
+# print("steps_df head:", steps_df.head())
+# print("cleaned_df head:", cleaned_df.head())
+
+# # Merge bolus_raw into steps_and_glucose_df, preserving steps_raw
+# parse_dataframe_to_csv(output_folder, df_to_save)
+
+
 
 # print(cleaned_df.head())
 # print(cleaned_df.dtypes)
---
+++ src/model_handling.py
@@ -0,0 +1,182 @@
+import os
+import keras
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+def read_csv_for_modeling(folder_path):
+    files = os.listdir(folder_path)
+    
+    csv_files = [os.path.join(folder_path, f) for f in files if f.endswith('.csv')]
+    df_list = [pd.read_csv(f) for f in csv_files]
+    combined_df = pd.concat(df_list, ignore_index=True)
+    print(combined_df.columns.tolist())
+    
+    return combined_df
+
+
+def create_sequences(data_array, target_array, time_steps=36, horizon=6):
+    """
+    Turns a 2D array into a 3D array of sequences.
+    time_steps=12 assumes 1 hour of history if data is every 5 mins.
+    """
+    X, y = [], []
+    
+    for i in range(len(data_array) - time_steps - horizon + 1):
+        
+        x_window = data_array[i : i + time_steps]
+       
+        y_target = target_array[i + time_steps + horizon - 1] 
+        
+        if np.isnan(x_window).any() or np.isnan(y_target):
+            continue
+        
+        X.append(x_window)
+        y.append(y_target)
+        
+    return np.array(X), np.array(y)
+
+def train_pateint_model(df, model_data_folder):
+    
+    patient_ids = df['patient_id'].unique()
+    print("Unique patient IDs:", patient_ids)
+       
+    results = []
+    for test_patient in patient_ids:
+        print(f"Processing patient_id: {test_patient}")
+        train_df = df[df['patient_id'] != test_patient].copy()
+        test_df = df[df['patient_id'] == test_patient].copy()
+        
+        # print("Train patients", train_df['patient_id'].unique())
+        # print("Test patient", test_df['patient_id'].unique())
+        
+        TIME_STEPS = 36
+        features = [col for col in train_df.columns if col not in ['patient_id', 'timestamp', 'glucose']]
+        target = 'glucose'
+        
+        scaler_X = StandardScaler()
+        X_train_scaled = scaler_X.fit_transform(train_df[features])
+        X_test_scaled = scaler_X.transform(test_df[features])
+
+        scaler_y = StandardScaler()
+        y_train_scaled = scaler_y.fit_transform(train_df[[target]])
+        y_test_scaled = scaler_y.transform(test_df[[target]])
+
+        train_data_combined = np.hstack([X_train_scaled, y_train_scaled])
+        test_data_combined = np.hstack([X_test_scaled, y_test_scaled])
+        
+        X_train, y_train = create_sequences(train_data_combined, y_train_scaled, time_steps=TIME_STEPS)
+        X_test, y_test = create_sequences(test_data_combined, y_test_scaled, time_steps=TIME_STEPS)
+        
+        print("Missing values in features:", train_df.isnull().sum().sum())
+        print("Missing values in target:", train_df[target].isnull().sum())
+        
+        print(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
+        print(f"X_test shape: {X_test.shape}, y_test shape: {y_test.shape}")
+        
+        print("====================================")
+        print("Building model...")
+        print("====================================")
+        
+        model = build_lstm_model(input_shape=(TIME_STEPS, len(features)+1))
+        
+        early_stop = keras.callbacks.EarlyStopping(
+        monitor='val_loss', 
+        patience=5, 
+        restore_best_weights=True
+        )   
+        
+        model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2, verbose=1, callbacks=[early_stop])
+        
+        y_pred_scaled = model.predict(X_test)
+        y_pred = scaler_y.inverse_transform(y_pred_scaled)
+        y_true = scaler_y.inverse_transform(y_test)
+        
+        plot_results(y_true, y_pred, model_data_folder, test_patient)
+        mae = mean_absolute_error(y_true, y_pred)
+        results.append(mae)
+        print(f"Final MAE for {test_patient}: {mae:.2f} mg/dL")
+        
+        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        print(f"Final RMSE for {test_patient}: {rmse:.2f} mg/dL")
+                
+    print("=========")
+    print("MODEL REUSLTS")
+    print("=========")
+    
+    print(results)
+
+    return results
+
+def build_lstm_model(input_shape, units=64, dropout_rate=0.3, learning_rate=0.001):
+    """
+    Builds and compiles a Sequential LSTM model.
+    
+    Parameters:
+    - input_shape: (time_steps, num_features) -> e.g., (12, 11)
+    - units: Number of 'neurons' in the LSTM layer.
+    - dropout_rate: Fraction of neurons to 'turn off' to prevent overfitting.
+    - learning_rate: How fast the model updates its weights.
+    """
+    
+    model = keras.models.Sequential()
+
+    model.add(keras.layers.LSTM(
+        units=units, 
+        input_shape=input_shape,
+    ))
+    
+    model.add(keras.layers.Dropout(dropout_rate))
+    
+    model.add(keras.layers.Dense(32, activation='relu')) 
+    
+    model.add(keras.layers.Dense(1))  # Output layer for regression
+    
+    model.summary()
+    
+    model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
+        loss='mean_squared_error',
+        metrics=['mean_absolute_error', keras.metrics.RootMeanSquaredError(name='rmse')]
+    )
+
+    return model
+
+
+def plot_results(expected_value, predicted_value, model_data_folder, test_patient):
+    plt.figure(figsize=(12,5))
+    
+    plt.plot(expected_value[:300], label="True")
+    plt.plot(predicted_value[:300], label="Predicted")
+    
+    plt.title("True vs Predicted Glucose")
+    plt.xlabel("Time Stpes")
+    plt.ylabel("Glucose (mg/dL)")
+    plt.legend()
+    
+    plt.savefig(model_data_folder + f"/patient_{test_patient}_1.png")
+    
+    plt.figure(figsize=(6,6))
+    
+    plt.scatter(expected_value, predicted_value, alpha=0.3)
+    
+    plt.plot([expected_value.min(), expected_value.max()], [expected_value.min(), expected_value.max()], color="red", linestyle="--")  
+    
+    plt.xlabel("True Glucose (mg/dL)")
+    plt.ylabel("Predicted Glucose (mg/dL)")
+    plt.title("True vs Predicted Glucose Scatter Plot")
+
+    plt.savefig(model_data_folder + f"/patient_{test_patient}_2.png")
+    
+    plt.figure(figsize=(6,6))
+    errors = expected_value - predicted_value
+    plt.hist(errors, bins=30, edgecolor='black')
+    plt.title("Distribution of Prediction Errors")
+    plt.xlabel("Prediction Error (mg/dL)")
+    plt.ylabel("Frequency")
+
+    plt.savefig(model_data_folder + f"/patient_{test_patient}_3.png")
+    
+    
